ff3918ffbe830e3d65c3d432acfabcda5c780b21
Maxime Schmitt <maxime.schmitt91@gmail.com>
2014-06-11 09:34:31 +0200

    Clean code and add comments

--- lex/src/preproc_utils.c
+++ lex/src/preproc_utils.c
@@ -24,10 +24,22 @@
 
 #include "preproc_utils.h"
 
+/**
+ * \brief The hash table used for data structure.
+ */
 static HT_hash_table *_hash_table = NULL;
+/**
+ * \brief The current identifier.
+ */
 static char* _current_id = NULL;
+/**
+ * \brief The current expression.
+ */
 static char* _current_expression = NULL;
 
+/**
+ * \brief Enum for initialize the array wave_reserved_id.
+ */
 enum{
     SQUARE_ROOT = 0,
     SIN,
@@ -54,6 +66,9 @@ enum{
     END_RES_ID
 };
 
+/**
+ * \brief An array which contains all reserverd words.
+ */
 static const char* wave_reserved_id[] = {
     [SQUARE_ROOT] = "sqrt",
     [SIN] = "sin",
@@ -85,12 +100,17 @@ void preproc_init(unsigned int size){
 
 char* preproc_concat_two_strings(const char* str1, const char* str2){
     size_t len1, len2;
+    if(str1 == NULL || str2 == NULL){
+        errno = EINVAL;
+        return NULL;
+    }
     len1 = strlen(str1);
     len2 = strlen(str2);
     char* new_one = calloc(len1 + len2 + 1, sizeof(char));
-    if( new_one != NULL )
+    if( new_one != NULL ){
         new_one = strcpy(new_one, str1);
-    new_one = strcat(new_one, str2);
+        new_one = strcat(new_one, str2);
+    }
     return new_one;
 }
 
@@ -101,17 +121,23 @@ void preproc_new_identifier(const char* id){
     _current_id = strdup(id);
 }
 
-static int preproc_add_value_with_previous_id(const char* value){
-    if(_current_id == NULL){
-        fprintf(stderr, "Aucun id n'a été enregistré précédement\n");
-        return 2;
-    }
-    return HT_add_element_string(_hash_table, _current_id, value);
+/**
+ * \brief Add the id with the value to the hash_table.
+ * \param value The value coresponding to the id.
+ * \retval 0 On success.
+ * \retval 2 On failure and errno is set appropriately.
+ */
+static inline int preproc_add_value_with_id(const char* id, const char* value){
+    return HT_add_element_string(_hash_table, id, value);
 }
 
-static int preproc_get_value_by_id(const char* identifier, char** value){
-    if( identifier == NULL )
-        return HT_get_element_string(_hash_table, _current_id, value);
+/**
+ * \brief Get the value associated with the identifier.
+ * \retval 0 On success.
+ * \retval 1 On not found.
+ * \retval 2 On failure and errno is set appropriately.
+ */
+static inline int preproc_get_value_by_id(const char* identifier, char** value){
     return HT_get_element_string(_hash_table, identifier, value);
 }
 
@@ -120,38 +146,44 @@ char* preproc_get_actual_identifier(void){
 }
 
 void preproc_clean(void){
-    if(_current_id != NULL)
+    if(_current_id != NULL){
         free(_current_id);
-    if(_hash_table != NULL)
+        _current_id = NULL;
+    }
+    if(_hash_table != NULL){
         HT_delete_pointer(_hash_table);
+        _hash_table = NULL;
+    }
+    if(_current_expression != NULL){
+        free(_current_expression);
+        _current_expression = NULL;
+    }
 }
 
 int preproc_add_current_id_with_current_expression(void){
-    if(_current_expression == NULL){
-        fprintf(stderr, "Aucune expression trouvée\n");
+    if(_current_expression == NULL || _current_id == NULL){
         return 1;
     }
-    if( preproc_add_value_with_previous_id(_current_expression) == 1 ){
-        fprintf(stderr, "The identifier \"%s\" is already used.\n", _current_id);
-        return 1;
+    if( preproc_add_value_with_id(_current_id, _current_expression) == 1 ){
+        return 2;
     }
     free(_current_expression);
     _current_expression = NULL;
-
-    char* value;
-    if(preproc_get_value_by_id(NULL, &value) == 0)
-        printf("Expression :\n%s\n", value);
-    else
-        printf("\nNON TROUVE\n"); /* add in the expression found buffer */
-    return 0;
+    free(_current_id);
+    _current_id = NULL;
 }
-
-int preproc_is_reserved_id(const char* id){
+/**
+ * \brief Test if the id is a reserved name.
+ * \param id A pointer to a null terminated string.
+ * \retval true If the id is reserved.
+ * \retval false Otherwise.
+ */
+static inline int preproc_is_reserved_id(const char* id){
     int i;
     for(i=0; i < END_RES_ID; i++)
         if( strcmp(id, wave_reserved_id[i]) == 0 )
-            return 1;
-    return 0;
+            return true;
+    return false;
 }
 
 void preproc_add_to_current_expression(const char* str){
@@ -164,23 +196,23 @@ void preproc_add_to_current_expression(const char* str){
         }
 }
 
-int preproc_test_or_add_identifier(const char* id_or_not){
-    if( preproc_is_reserved_id(id_or_not) ){
+int preproc_add_id_definition_to_current_expression(const char* id_or_not){
+    if( preproc_is_reserved_id(id_or_not) ){ // If the identidier is reserved copy it integrally to the expression.
         preproc_add_to_current_expression(id_or_not);
     }
     else{
         char* value;
         int retval = preproc_get_value_by_id(id_or_not, &value);
-        if(retval == 0){
+        if(retval == 0){ // If the identifier exists add its definition.
             preproc_add_to_current_expression(value);
         }
-        else{
-            fprintf(stderr, "This identifier does not exist %s.\n", id_or_not);
+        else{ // The identifier does not exist.
             return 1;
         }
     }
     return 0;
 }
+
 int preproc_check_identifier(char* identifier, char** associated_value){
     char* val;
     if(preproc_is_reserved_id(identifier))
