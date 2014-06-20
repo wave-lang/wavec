5ffdcc42b66386c4ac9d48d166b9f25d90822b50
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2014-06-20 16:49:46 +0200

    update wave_types doc

--- include/wave/common/wave_types.h
+++ include/wave/common/wave_types.h
@@ -5,6 +5,7 @@
  * \author SCHMITT Maxime
  * \date 2014
  * \copyright MIT License
+ * This file defines utilities for Wave types.
  */
 /* The MIT License (MIT)
  *
@@ -46,6 +47,12 @@
 /**
  * \class wave_bool
  * \brief Wave boolean.
+ *
+ * wave_bool is the boolean type of Wave. It consists of two values:
+ * - ::WAVE_BOOL_TRUE
+ * - ::WAVE_BOOL_FALSE
+ *
+ * The values ::WAVE_BOOL_TRUE and ::WAVE_BOOL_FALSE are equivalent, respectively, to the values \c true and \c false of stdbool.h.
  */
 /**
  * \brief Wave boolean.
@@ -55,6 +62,8 @@ typedef bool wave_bool;
 /**
  * \class wave_int
  * \brief Wave integer.
+ *
+ * wave_int is the integer type of Wave.
  */
 /**
  * \brief Wave integer.
@@ -64,6 +73,8 @@ typedef int wave_int;
 /**
  * \class wave_float
  * \brief Wave float.
+ *
+ * wave_float is the floating point type of Wave.
  */
 /**
  * \brief Wave float.
@@ -73,6 +84,8 @@ typedef double wave_float;
 /**
  * \class wave_char
  * \brief Wave character.
+ *
+ * wave_char is the character type of Wave.
  */
 /**
  * \brief Wave character.
@@ -82,14 +95,22 @@ typedef char wave_char;
 /**
  * \class wave_string
  * \brief Wave string.
+ * \sa wave_char
+ *
+ * wave_string is the string type of Wave. const_wave_string is the \c const
+ * variant of wave_string.
+ *
+ * Internally, wave_string and const_wave_string are composed of several wave_char characters.
  */
 /**
  * \brief Wave string.
+ * \sa const_wave_string
  */
 typedef wave_char * wave_string;
 
 /**
  * \brief Const wave_string.
+ * \sa wave_string
  */
 typedef const wave_char * const_wave_string;
 
@@ -167,14 +188,13 @@ static const wave_int WAVE_INT_MAX = INT_MAX;
  * \param f A wave_float.
  * \return The floor of the float.
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_int wave_int_from_wave_float (wave_float f);
 
 /**
- * \brief Convert a null terminated string to a wave_int.
+ * \brief Convert the initial portion of a null terminated string to a wave_int.
  * \param str A null terminated string.
- * \return The wave_int associated.
+ * \return The converted value.
  * \relatesalso wave_int
  */
 wave_int wave_int_from_string (const char * str);
@@ -196,7 +216,7 @@ wave_int wave_int_unary_plus (wave_int a);
 wave_int wave_int_unary_minus (wave_int a);
 
 /**
- * \brief \c Increment an wave_int.
+ * \brief \c Increment a wave_int.
  * \param a A wave_int.
  * \return a+1.
  * \relatesalso wave_int
@@ -204,7 +224,7 @@ wave_int wave_int_unary_minus (wave_int a);
 wave_int wave_int_increment (wave_int a);
 
 /**
- * \brief \c Decrement an wave_int.
+ * \brief \c Decrement a wave_int.
  * \param a A wave_int.
  * \return a-1.
  * \relatesalso wave_int
@@ -212,65 +232,58 @@ wave_int wave_int_increment (wave_int a);
 wave_int wave_int_decrement (wave_int a);
 
 /**
- * \brief The \c square \c root of an wave_int.
+ * \brief The \c square \c root of a wave_int.
  * \param a A wave_int.
  * \return sqrt(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_sqrt (wave_int a);
 
 /**
- * \brief The \c sin of an wave_int.
+ * \brief The \c sin of a wave_int.
  * \param a A wave_int.
  * \return sin(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_sin (wave_int a);
 
 /**
- * \brief The \c cos of an wave_int.
+ * \brief The \c cos of a wave_int.
  * \param a A wave_int.
  * \return cos(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_cos (wave_int a);
 
 /**
- * \brief The \c log of an wave_int.
+ * \brief The \c log of a wave_int.
  * \param a A wave_int.
  * \return log(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_log (wave_int a);
 
 /**
- * \brief The \c exp of an wave_int.
+ * \brief The \c exp of a wave_int.
  * \param a A wave_int.
  * \return exp(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_exp (wave_int a);
 
 /**
- * \brief The \c ceil of an wave_int.
+ * \brief The \c ceil of a wave_int.
  * \param a A wave_int.
  * \return ceil(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_ceil (wave_int a);
 
 /**
- * \brief The \c floor of an wave_int.
+ * \brief The \c floor of a wave_int.
  * \param a A wave_int.
  * \return floor(a).
  * \relatesalso wave_int
- * \relatesalso wave_float
  */
 wave_float wave_int_floor (wave_int a);
 
@@ -332,7 +345,7 @@ wave_int wave_int_divide (wave_int a, wave_int b);
  * \brief \c mod operation for wave_int.
  * \param a First wave_int.
  * \param b Second wave_int.
- * \return a mod(b).
+ * \return a mod b.
  * \relatesalso wave_int
  */
 wave_int wave_int_mod (wave_int a, wave_int b);
@@ -357,7 +370,6 @@ wave_bool wave_int_equals (wave_int a, wave_int b);
  * \retval true True.
  * \retval false False.
  * \relatesalso wave_int
- * \relatesalso wave_bool
  */
 wave_bool wave_int_differs (wave_int a, wave_int b);
 
@@ -369,7 +381,6 @@ wave_bool wave_int_differs (wave_int a, wave_int b);
  * \retval true True.
  * \retval false False.
  * \relatesalso wave_int
- * \relatesalso wave_bool
  */
 wave_bool wave_int_lesser_or_equals (wave_int a, wave_int b);
 
@@ -381,7 +392,6 @@ wave_bool wave_int_lesser_or_equals (wave_int a, wave_int b);
  * \retval true True.
  * \retval false False.
  * \relatesalso wave_int
- * \relatesalso wave_bool
  */
 wave_bool wave_int_greater_or_equals (wave_int a, wave_int b);
 
@@ -393,7 +403,6 @@ wave_bool wave_int_greater_or_equals (wave_int a, wave_int b);
  * \retval true True.
  * \retval false False.
  * \relatesalso wave_int
- * \relatesalso wave_bool
  */
 wave_bool wave_int_greater (wave_int a, wave_int b);
 
@@ -405,7 +414,6 @@ wave_bool wave_int_greater (wave_int a, wave_int b);
  * \retval true True.
  * \retval false False.
  * \relatesalso wave_int
- * \relatesalso wave_bool
  */
 wave_bool wave_int_lesser (wave_int a, wave_int b);
 
@@ -417,7 +425,6 @@ wave_bool wave_int_lesser (wave_int a, wave_int b);
  * \brief The cast from wave_float to wave_int.
  * \param i The wave_int.
  * \return A wave_float.
- * \relatesalso wave_int
  * \relatesalso wave_float
  */
 wave_float wave_float_from_wave_int (wave_int i);
@@ -579,7 +586,6 @@ wave_float wave_float_mod (wave_float a, wave_float b);
  * \param b Second wave_float.
  * \return a == b.
  * \relatesalso wave_float
- * \relatesalso wave_bool
  */
 wave_bool wave_float_equals (wave_float a, wave_float b);
 
@@ -589,7 +595,6 @@ wave_bool wave_float_equals (wave_float a, wave_float b);
  * \param b Second wave_float.
  * \return a != b.
  * \relatesalso wave_float
- * \relatesalso wave_bool
  */
 wave_bool wave_float_differs (wave_float a, wave_float b);
 
@@ -599,7 +604,6 @@ wave_bool wave_float_differs (wave_float a, wave_float b);
  * \param b Second wave_float.
  * \return a <= b.
  * \relatesalso wave_float
- * \relatesalso wave_bool
  */
 wave_bool wave_float_lesser_or_equals (wave_float a, wave_float b);
 
@@ -609,7 +613,6 @@ wave_bool wave_float_lesser_or_equals (wave_float a, wave_float b);
  * \param b Second wave_float.
  * \return a >= b.
  * \relatesalso wave_float
- * \relatesalso wave_bool
  */
 wave_bool wave_float_greater_or_equals (wave_float a, wave_float b);
 
@@ -619,7 +622,6 @@ wave_bool wave_float_greater_or_equals (wave_float a, wave_float b);
  * \param b Second wave_float.
  * \return a > b.
  * \relatesalso wave_float
- * \relatesalso wave_bool
  */
 wave_bool wave_float_greater (wave_float a, wave_float b);
 
@@ -629,7 +631,6 @@ wave_bool wave_float_greater (wave_float a, wave_float b);
  * \param b Second wave_float.
  * \return a < b.
  * \relatesalso wave_float
- * \relatesalso wave_bool
  */
 wave_bool wave_float_lesser (wave_float a, wave_float b);
 
@@ -695,7 +696,6 @@ wave_char wave_char_max (wave_char a, wave_char b);
  * \param b Second wave_char.
  * \return a == b
  * \relatesalso wave_char
- * \relatesalso wave_bool
  */
 wave_bool wave_char_equals (wave_char a, wave_char b);
 
@@ -705,7 +705,6 @@ wave_bool wave_char_equals (wave_char a, wave_char b);
  * \param b Second wave_char.
  * \return a != b
  * \relatesalso wave_char
- * \relatesalso wave_bool
  */
 wave_bool wave_char_differs (wave_char a, wave_char b);
 
@@ -715,7 +714,6 @@ wave_bool wave_char_differs (wave_char a, wave_char b);
  * \param b Second wave_char.
  * \return a <= b
  * \relatesalso wave_char
- * \relatesalso wave_bool
  */
 wave_bool wave_char_lesser_or_equals (wave_char a, wave_char b);
 
@@ -725,7 +723,6 @@ wave_bool wave_char_lesser_or_equals (wave_char a, wave_char b);
  * \param b Second wave_char.
  * \return a >= b
  * \relatesalso wave_char
- * \relatesalso wave_bool
  */
 wave_bool wave_char_greater_or_equals (wave_char a, wave_char b);
 
@@ -735,7 +732,6 @@ wave_bool wave_char_greater_or_equals (wave_char a, wave_char b);
  * \param b Second wave_char.
  * \return a > b
  * \relatesalso wave_char
- * \relatesalso wave_bool
  */
 wave_bool wave_char_greater (wave_char a, wave_char b);
 
@@ -745,7 +741,6 @@ wave_bool wave_char_greater (wave_char a, wave_char b);
  * \param b Second wave_char.
  * \return a < b
  * \relatesalso wave_char
- * \relatesalso wave_bool
  */
 wave_bool wave_char_lesser (wave_char a, wave_char b);
 
@@ -775,8 +770,9 @@ wave_int wave_char_code (wave_char c);
  * \brief Concatenate two wave_string.
  * \param a A wave_string.
  * \param b A wave_string.
- * \return "ab"
+ * \return The resulting wave_string.
  * \relatesalso wave_string
+ * \warning The resulting wave_string is allocated and thus shall be freed once uneeded.
  */
 wave_string wave_string_plus (const_wave_string a, const_wave_string b);
 
@@ -786,6 +782,7 @@ wave_string wave_string_plus (const_wave_string a, const_wave_string b);
  * \param b A wave_string.
  * \return min(a,b)
  * \relatesalso wave_string
+ * \warning The resulting wave_string is allocated and thus shall be freed once uneeded.
  */
 wave_string wave_string_min (const_wave_string a, const_wave_string b);
 
@@ -795,6 +792,7 @@ wave_string wave_string_min (const_wave_string a, const_wave_string b);
  * \param b A wave_string.
  * \return max(a,b)
  * \relatesalso wave_string
+ * \warning The resulting wave_string is allocated and thus shall be freed once uneeded.
  */
 wave_string wave_string_max (const_wave_string a, const_wave_string b);
 
@@ -804,7 +802,6 @@ wave_string wave_string_max (const_wave_string a, const_wave_string b);
  * \param b A wave_string.
  * \return a == b
  * \relatesalso wave_string
- * \relatesalso wave_bool
  */
 wave_bool wave_string_equals (const_wave_string a, const_wave_string b);
 
@@ -814,7 +811,6 @@ wave_bool wave_string_equals (const_wave_string a, const_wave_string b);
  * \param b A wave_string.
  * \return a != b
  * \relatesalso wave_string
- * \relatesalso wave_bool
  */
 wave_bool wave_string_differs (const_wave_string a, const_wave_string b);
 
@@ -824,7 +820,6 @@ wave_bool wave_string_differs (const_wave_string a, const_wave_string b);
  * \param b A wave_string.
  * \return a <= b
  * \relatesalso wave_string
- * \relatesalso wave_bool
  */
 wave_bool wave_string_lesser_or_equals (const_wave_string a, const_wave_string b);
 
@@ -834,7 +829,6 @@ wave_bool wave_string_lesser_or_equals (const_wave_string a, const_wave_string b
  * \param b A wave_string.
  * \return a >= b
  * \relatesalso wave_string
- * \relatesalso wave_bool
  */
 wave_bool wave_string_greater_or_equals (const_wave_string a, const_wave_string b);
 
@@ -844,7 +838,6 @@ wave_bool wave_string_greater_or_equals (const_wave_string a, const_wave_string 
  * \param b A wave_string.
  * \return a > b
  * \relatesalso wave_string
- * \relatesalso wave_bool
  */
 wave_bool wave_string_greater (const_wave_string a, const_wave_string b);
 
@@ -854,7 +847,6 @@ wave_bool wave_string_greater (const_wave_string a, const_wave_string b);
  * \param b A wave_string.
  * \return a < b
  * \relatesalso wave_string
- * \relatesalso wave_bool
  */
 wave_bool wave_string_lesser (const_wave_string a, const_wave_string b);
 
@@ -865,7 +857,8 @@ wave_bool wave_string_lesser (const_wave_string a, const_wave_string b);
  * \return pointer to the resulting string \c destination.
  * \warning \c destination must have enough space for the result.
  * \warning The strings may not overlap.
- * This function mimics \c strcat from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strcat from \c string.h, please read its manual for further information.
  */
 wave_string wave_string_cat (wave_string restrict destination, const_wave_string restrict source);
 
@@ -874,7 +867,8 @@ wave_string wave_string_cat (wave_string restrict destination, const_wave_string
  * \param a First string.
  * \param b Second string.
  * \return a wave_int less than, equal to, or greater than \c 0 if \c a is found, respectively, to be less than, to match, or be grater than \c b.
- * This function mimics \c strcmp from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strcmp from \c string.h, please read its manual for further information.
  */
 wave_int wave_string_compare (const_wave_string a, const_wave_string b);
 
@@ -885,7 +879,8 @@ wave_int wave_string_compare (const_wave_string a, const_wave_string b);
  * \return pointer to the resulting string \c destination.
  * \warning \c destination must have enough space for the result.
  * \warning The strings may not overlap.
- * This function mimics \c strcpy from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strcpy from \c string.h, please read its manual for further information.
  */
 wave_string wave_string_copy (wave_string restrict destination, const_wave_string restrict source);
 
@@ -893,7 +888,8 @@ wave_string wave_string_copy (wave_string restrict destination, const_wave_strin
  * \brief Calculate the length of a wave_string.
  * \param s string.
  * \return The size of the string, excluding the terminating null character.
- * This function mimics \c strlen from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strlen from \c string.h, please read its manual for further information.
  */
 size_t wave_string_length (const_wave_string s);
 
@@ -903,7 +899,8 @@ size_t wave_string_length (const_wave_string s);
  * \param c character.
  * \retval NULL if the character is not found.
  * \return pointer to the matched character if found.
- * This function mimics \c strchr from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strchr from \c string.h, please read its manual for further information.
  */
 wave_string wave_string_first_character (const_wave_string s, wave_char c);
 
@@ -913,7 +910,8 @@ wave_string wave_string_first_character (const_wave_string s, wave_char c);
  * \param c character.
  * \retval NULL if the character is not found.
  * \return pointer to the matched character if found.
- * This function mimics \c strrchr from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strrchr from \c string.h, please read its manual for further information.
  */
 wave_string wave_string_last_character (const_wave_string s, wave_char c);
 
@@ -923,7 +921,8 @@ wave_string wave_string_last_character (const_wave_string s, wave_char c);
  * \param needle Needle.
  * \retval NULL if the substring is not found.
  * \return pointer to the beginning of the substring if found.
- * This function mimics \c strstr from \c string.h, please read its manual for further information.
+ * \relatesalso wave_string
+ * \note This function mimics \c strstr from \c string.h, please read its manual for further information.
  */
 wave_string wave_string_sub_string (const_wave_string haystack, const_wave_string needle);
 
@@ -933,6 +932,7 @@ wave_string wave_string_sub_string (const_wave_string haystack, const_wave_strin
  * \retval NULL if the allocation of new memory failed
  * \return pointer to the duplicate in case of success.
  * \note The memory for the duplicate is obtained using \c malloc and thus shall be freed using \c free.
+ * \relatesalso wave_string
  */
 wave_string wave_string_duplicate (const_wave_string s);
 
